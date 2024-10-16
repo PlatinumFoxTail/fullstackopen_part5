2515c36e5cfbaa27ba0a767593ddf3a3511633d2
Martin Häggblom <martin.haggblom@gmail.com>
2024-10-16 14:27:23 +0300

    submit 5.10: Blog List Frontend, step 10

--- src/App.jsx
+++ src/App.jsx
@@ -95,9 +95,12 @@ const App = () => {
 
   const blogList = () => (
     <div>
-      {blogs.map(blog =>
+      {blogs
+        .sort((blog1, blog2) => blog2.likes - blog1.likes)
+        .map(blog => (
         <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-      )}
+        ))
+      }
     </div>
   )
 
