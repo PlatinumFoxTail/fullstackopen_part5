e9a1f279b624c43beb994bfaabfc954e441c09ac
Martin Häggblom <martin.haggblom@gmail.com>
2024-10-17 07:46:47 +0300

    submit 5.14: Blog List Tests, step 2

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author, but not url or likes', () => {
-  const blog = {
-    title: 'Blogs 101',
-    author: 'Billy Blog',
-    url: 'www.blogs101.com',
-    likes: 100,
-    user: {
-      username: 'backtobasicblogs',
-      name: 'Average Joe',
-    },
-  }
+const blog = {
+  title: 'Blogs 101',
+  author: 'Billy Blog',
+  url: 'www.blogs101.com',
+  likes: 100,
+  user: {
+    username: 'backtobasicblogs',
+    name: 'Average Joe',
+  },
+}
 
+test('renders title and author, but not url or likes', () => {
   const { container } = render(<Blog blog={blog} />)
 
   const elementTitleAuthor = container.querySelector('.blog-summary')
@@ -22,4 +23,15 @@ test('renders title and author, but not url or likes', () => {
 
   expect(container.querySelector('.blog-url')).toBeNull()
   expect(container.querySelector('.blog-likes')).toBeNull()
+})
+
+test('renders url and likes after clicking view button', async () => {
+  const { container } = render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(container.querySelector('.blog-url')).toBeDefined()
+  expect(container.querySelector('.blog-likes')).toBeDefined()
 })
