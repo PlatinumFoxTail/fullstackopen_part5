f052a77038762d5f80106dc96731a968e67cee9a
Martin Häggblom <martin.haggblom@gmail.com>
2024-10-17 08:08:23 +0300

    submit 5.15: Blog List Tests, step 3

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -35,3 +35,22 @@ test('renders url and likes after clicking view button', async () => {
   expect(container.querySelector('.blog-url')).toBeDefined()
   expect(container.querySelector('.blog-likes')).toBeDefined()
 })
+
+test('clicking the like button twice calls event handler twice', async () => {
+  const mockHandler = vi.fn()
+
+  render(
+    <Blog blog={blog} handleLike={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
